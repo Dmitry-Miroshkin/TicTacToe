9408b3b09d9409851763d9d102b78503bd77671c
Dmtry-Miroshkin <dmm@mail.ru>
2019-09-06 15:16:43 +0300

    сделано до хода компа

---
+++ MAIN/src/Main_Game.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Main_Game {
+    private static int SIZE = 3;
+    private static char[][] map;
+    private static final char DOT_EMPTY = '_';
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = 'O';
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
+            if (isEndGame(DOT_X)) {
+                System.out.println("Игра закончена");
+                break;
+            }
+/*            computerTurn();
+            if (isEndGame(DOT_X)) {
+            System.out.println("Игра закончена");
+                break;
+            }*/
+
+        }
+    }
+
+    private static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    // Вывод поля на экран
+    private static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+    private static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты ячейки через пробел (X Y)");
+            y = scanner.nextInt() - 1; // Считывание номера строки
+            x = scanner.nextInt() - 1; // Считывание номера столбца
+        }
+        while (!isCellValid(x, y));
+
+        map[y][x] = DOT_X;
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        boolean result = true;
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+            result = false;
+        }
+
+        if (map[y][x] != DOT_EMPTY) {
+            result = false;
+        }
+        return result;
+    }
+
+    private static boolean isEndGame(char playerSymbol) {
+        boolean result = false;
+        printMap();
+
+        if (checkWin(playerSymbol)) {
+            System.out.println("Победили " + playerSymbol);
+            result = true;
+        }
+        if (mapIsFull()) {
+            System.out.println("Ничья!");
+            result = true;
+        }
+        return result;
+    }
+
+    private static boolean mapIsFull() {
+        boolean result = true;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) result = false;
+            }
+        }
+        return result;
+    }
+
+    private static boolean checkWin(char playerSymbol) {
+        boolean result = false;
+        if (
+                (map[0][0] == playerSymbol && map[0][1] == playerSymbol && map[0][2] == playerSymbol) ||
+                        (map[1][0] == playerSymbol && map[1][1] == playerSymbol && map[1][2] == playerSymbol) ||
+                        (map[2][0] == playerSymbol && map[2][1] == playerSymbol && map[2][2] == playerSymbol) ||
+                        (map[0][0] == playerSymbol && map[1][0] == playerSymbol && map[2][0] == playerSymbol) ||
+                        (map[0][1] == playerSymbol && map[1][1] == playerSymbol && map[2][1] == playerSymbol) ||
+                        (map[0][2] == playerSymbol && map[1][2] == playerSymbol && map[2][2] == playerSymbol) ||
+                        (map[0][0] == playerSymbol && map[1][1] == playerSymbol && map[2][2] == playerSymbol) ||
+                        (map[2][0] == playerSymbol && map[1][1] == playerSymbol && map[0][2] == playerSymbol)
+        )
+            result = true;
+
+
+        return result;
+
+    }
+}
